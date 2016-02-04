891bcab0b2b5a6528499dd33e6a38560d270d8ac
Brian Holland <beehollander@gmail.com>
2016-02-04 22:47:37 +0000

    Done.

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,29 @@
 def even_next(n)
+  return n/2
 end
 
 def odd_next(n)
+  return (3*n) + 1
 end
 
 def next_value(n)
+  return n%2==0 ? even_next(n) : odd_next(n)
 end
 
 def collatz(n)
+  latz = Array.new()
+  while n!=1
+    latz.push(n)
+    n = next_value(n)
+  end
+  latz.push(n)
+  return latz
 end
 
 def longest_collatz
+  million = (1..1e6).to_a
+  million = million.map{|a| collatz(a)}
+  million = million.map{|a| a.length}
+  index = million.rindex(million.max)
+  return index+1
 end
---
+++ lib/testing.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/project_euler_collatz'
+
+puts "odd_next(5): #{odd_next(5)}"
+puts "even_next(6): #{even_next(6)}"
+puts "collatz(13): #{collatz(13).join(" ")}"
+puts "collatz(8): #{collatz(8).join(" ")}"
+#puts "longest_collatz(100): #{longest_collatz}"
